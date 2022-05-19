900bdb1d937915b2e80163ccf5e1f2c7b041ecd9
Finnialator <55963012+Finnialator@users.noreply.github.com>
2022-05-19 13:33:41 +1000

    Some projects

---
+++ LearningVariables/LearningVariables/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningVariables
+{
+    internal class Program
+    {
+        static void Main()
+        {         
+            string greet ="Hello, user!";
+            int playerScore = 0;
+            string displayScore = "Your score currently is" + playerScore + ".";
+
+            Console.WriteLine(greet);
+            Console.WriteLine(displayScore);
+            Console.ReadKey();
+
+            string greeting = "Hello, user! The game's start condition is: ";
+            bool gameStarted = false;
+            string displayGameState = greeting + gameStarted;
+            Console.WriteLine(displayGameState);
+
+            gameStarted = true;
+            displayGameState = greeting + gameStarted;
+            Console.WriteLine(displayGameState);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ VariablesExercise/VariablesExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesExercise
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            float playerHealth;
+            bool inCombat = false;
+            float monsterHealth = 100;
+            playerHealth = 100;
+            monsterHealth = 50;
+            string battleStartText = "Battle has commenced";
+            string playerHealthText = "Player has " + playerHealth + " Health";
+            string monsterHealthText = "Monster has " + monsterHealth + " Health";
+
+            Console.WriteLine(battleStartText);
+            Console.WriteLine(playerHealthText);
+            Console.WriteLine(monsterHealthText);
+            Console.ReadKey();
+        }
+    }
+}
